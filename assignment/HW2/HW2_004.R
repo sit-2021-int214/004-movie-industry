5f72431aa937c5d3e8b8a1583434649ec6aefca0
tan <t.songglod@gmail.com>
2021-08-23 19:55:07 +0700

    [EDIT] edit .R file and README.md

--- assignment/HW2/HW2_004.R
+++ assignment/HW2/HW2_004.R
@@ -3,14 +3,32 @@ lapply(packages, library, character.only = T)
 
 head(survey)
 # 1
+# code
+
 # 2
+# code
+
 # 3
+# code
+
 # 4
+# code
+
 # 5
+# code
 
 head(cats)
 # 1
+# code
+
 # 2
+# code
+
 # 3
+# code
+
 # 4
+# code
+
 # 5
+# code
