ae99bfec7ccb13eda5dd8611d11aae935b8f1a4c
tan <t.songglod@gmail.com>
2021-08-25 00:54:50 +0700

    [EDIT] add item 1 in survey section & edit README.md

--- assignment/HW2/HW2_004.R
+++ assignment/HW2/HW2_004.R
@@ -1,11 +1,23 @@
 packages <- c("DescTools", "MASS")
 lapply(packages, library, character.only = T)
 
-head(survey)
-# 1
-# code
+View(survey)
+# 1 find something ... i don't know ha ha
+male_wHnd <- table(survey[survey$Sex == "Male", "W.Hnd"])
+female_wHnd <- table(survey[survey$Sex == "Female", "W.Hnd"])
+df_wHnd <- rbind(male_wHnd, female_wHnd)
+total <- colSums(df_wHnd)
+df_wHnd <- rbind(df_wHnd, total)
+df_wHnd <- as.data.frame(df_wHnd)
+df_wHnd <- cbind.data.frame(df_wHnd, rowSums(df_wHnd))
+row.names(df_wHnd)[1:2] <- c("male", "female")
+colnames(df_wHnd)[length(df_wHnd)] <- "total"
+total_percentage <- round((total/sum(total)) * 100, digits = 2)
+print(df_wHnd)
+cat("total Left(%):", total_percentage["Left"])
+cat("total Right(%):", total_percentage["Right"])
 
-# 2
+# 2F
 # code
 
 # 3
@@ -17,7 +29,7 @@ head(survey)
 # 5
 # code
 
-head(cats)
+View(cats)
 # 1
 # code
 
