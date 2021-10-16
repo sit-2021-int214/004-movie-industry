c63d5ad5e04e8fc3f8412f55043f6820c5f5a8ed
tan <t.songglod@gmail.com>
2021-10-17 03:56:38 +0700

    [EDIT] add new pck and new plot style

--- assignments/HW4/HW04_63130500042.R
+++ assignments/HW4/HW04_63130500042.R
@@ -1,23 +1,45 @@
-packages <- c("dplyr","readr","ggplot2")
+packages <- c("dplyr","readr","ggplot2","plotly")
 lapply(packages, library, character.only = T)
 
 dt <- read_csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/prog_book.csv")
 
-View(dt)
 glimpse(dt)
 
 print(colSums(is.na(dt)))
 # no N/A value
 
-dt %>% filter(Number_Of_Pages < 1500 & Price < 150) %>% 
-    ggplot() +
-    aes(x = Number_Of_Pages, y = Price) + 
-    geom_point(size = 2) + 
-    aes(color = Rating) + 
-    facet_wrap("Type")
+# 1
 
-dt %>% filter(Number_Of_Pages < 1500 & Price < 150) %>% 
+# 2
+# 3
+# 4
+# 5
+# 6
+
+hist_p <- dt %>% 
+    filter(Price < 150) %>% 
     ggplot() +
-    aes(x = Number_Of_Pages, y = Price) + 
+    aes(x = Price) + 
+    geom_histogram(bins = 10, alpha = 0.7) +
+    aes(fill = Type) +
+    geom_vline(xintercept = mean(dt$Price), linetype = "dashed") +
+    ggtitle("test") +
+    theme_bw()
+hist_p
+ggplotly(hist_p)
+
+scatter_p <- dt %>%
+    filter(Number_Of_Pages < 1500 & Price < 150) %>%
+    filter(Type == "Hardcover" | Type == "Paperback") %>%
+    ggplot() +
+    aes(x = Price, y = Number_Of_Pages) +
+    aes(color = Rating, size = Reviews) +
     geom_point() + 
-    aes(color = Type)
+    scale_color_distiller(palette = "RdPu") +
+    facet_wrap(~Type, scales = "free") +
+    geom_vline(xintercept = mean(dt$Price), linetype = "dashed") +
+    geom_hline(yintercept = mean(dt$Number_Of_Pages), linetype = "dashed") +
+    ggtitle("test") +
+    theme_bw()
+scatter_p
+ggplotly(scatter_p)
