24de6fc65203fb474b7f956ce98e6da05f6ac02d
hunnymc <69097896+hunnymc@users.noreply.github.com>
2021-10-17 19:37:28 +0700

    Update WS01_63130500048.R

--- assignments/workshop_01/WS01_63130500048/WS01_63130500048.R
+++ assignments/workshop_01/WS01_63130500048/WS01_63130500048.R
@@ -1,10 +1,26 @@
-# Student_ID
+# 63130500048
 # Step 0
-library(dplyr)
+library("dplyr","readr","assertive","stringr")
 
+sat_score <- read_csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/SAT_original.csv")
+View(sat_score)
 # Explore
 
 # xxxx
+
+#Define a question
+#1. How many observation of this dataset (before cleaning) ?
+
+#2. Are there duplicate data sets ? (If have duplicate data, list the data that duplicate)
+
+#3. How many distinct school in this dataset ? (Know after drop duplicate data)
+
+#4. What is min, max, average, quartile of each part in SAT ?
+
+#5. What is min, max, average, quartile of total score in SAT ?
+
+#6. Which school is get highest SAT score ?
+
 
 
 
