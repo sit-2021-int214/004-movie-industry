6c281dd47d49235926a39c2030aaab5bdb84f542
hunnymc <69097896+hunnymc@users.noreply.github.com>
2021-10-17 18:02:06 +0700

    add folder WS_01_63130500048

---
+++ assignments/workshop_01/WS01_63130500048/WS01_63130500048.R
@@ -0,0 +1,44 @@
+# Student_ID
+# Step 0
+library(dplyr)
+
+# Explore
+
+# xxxx
+
+
+
+# USEFUL FUNCTION (After finished please remove it) -------------------------
+## Stat
+sum()
+min()
+mean()
+max()
+
+## Explore
+head()      # See 6 observation (rows)
+str()       # Structure of object
+summary()
+
+## dplyr package
+glimpse()
+select()
+filter()
+arrange()
+mutate()
+group_by()
+summarise()
+count()
+
+## Cleaning
+as.vector()
+as.numeric()
+na.omit()
+is.na()
+replace()
+distinct()
+duplicated()
+
+#assertive package
+assert_is_numeric()
+assert_all_are_in_closed_range()
