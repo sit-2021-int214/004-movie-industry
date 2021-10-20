a845397ba296ee5ff14713a66f6a9da66d0fb13a
tan <t.songglod@gmail.com>
2021-10-20 18:44:17 +0700

    [EDIT] new section for ez read

--- movie_industry/data_cleaning/cleaning_stage.R
+++ movie_industry/data_cleaning/cleaning_stage.R
@@ -9,16 +9,20 @@ glimpse(original)
 
 original <- original %>%
     separate(released, c("released.date", "released.country"), " \\(") %>%
-    mutate(released.country = str_remove(released.country, "\\)")) %>%
+    mutate(released.country = str_remove(released.country, "\\)"))
+
+original <- original %>%
+    mutate_at(c("rating", "writer", "star", "country", "company", "released.country"),
+              ~replace_na(., "unknown"))
+
+original <- mutate_if(original, is.numeric, ~replace_na(., 0))
+
+original <- original %>%
     mutate(released.date = as.Date(released.date, format = "%B %d, %Y")) %>%
     mutate(year = as.factor(format(year, format = "%Y"))) %>%
-    mutate_at(c("rating", "writer", "star", "country", "company", "released.country"),
-              ~replace_na(., "unknown")) %>%
     mutate(released.country = as.factor(released.country)) %>%
     mutate_at(.vars = c("genre", "rating", "country"), .funs = as.factor) %>%
     mutate_at(.vars = c("votes", "runtime"), .funs = as.integer)
-
-original <- mutate_if(original, is.numeric, ~replace_na(., 0))
 
 summary(original)
 glimpse(original)
