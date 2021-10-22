671aedf6db2ad4eb7346772eb3a8eb0562bae59b
tan <t.songglod@gmail.com>
2021-10-22 17:38:03 +0700

    [EDIT] update R file

--- assignments/HW4/HW04_63130500042/HW04_63130500042.R
+++ assignments/HW4/HW04_63130500042/HW04_63130500042.R
@@ -18,19 +18,31 @@ colSums(is.na(dt))
 summary(dt)
 
 # 1
+dt %>%
+    dplyr::filter(Rating > quantile(dt$Rating, 0.75)) %>%
+    dplyr::select(Type) %>%
+    dplyr::group_by(Type) %>%
+    count(.) %>%
+    dplyr::rename(amount = n) %>%
+    dplyr::arrange(desc(amount))
+    
 # 2
+dt
+    
 # 3
 # 4
 # 5
 # 6
 
 hist_p <- dt %>% 
-    filter(Price < 150) %>% 
+    # filter(Price < 150) %>% 
     ggplot() +
-    aes(x = Price) + 
-    geom_histogram(bins = 10, alpha = 0.7) +
+    aes(x = Rating) + 
+    geom_histogram(binwidth = 0.2) +
     aes(fill = Type) +
-    geom_vline(xintercept = mean(dt$Price), linetype = "dashed") +
+    geom_vline(xintercept = mean(dt$Rating), linetype = "dashed") +
+    scale_x_continuous(breaks = seq(min(dt$Rating), max(dt$Rating), 0.2)) +
+    scale_y_continuous(breaks = seq(0, 100, 10)) +
     ggtitle("test") +
     theme_bw()
 hist_p
@@ -47,6 +59,8 @@ scatter_p <- dt %>%
     facet_wrap(~Type) +
     geom_hline(yintercept = mean(dt$Price), linetype = "dashed", alpha = 0.5) +
     geom_vline(xintercept = mean(dt$Number_Of_Pages), linetype = "dashed", alpha = 0.5) +
+    scale_x_continuous(breaks = seq(0, 1200, 200)) +
+    scale_y_continuous(breaks = seq(0, 150, 10)) +
     stat_ellipse() +
     ggtitle("test") +
     theme_bw()
