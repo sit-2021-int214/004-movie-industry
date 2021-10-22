d3fc08bdff235e12d530b36921d61e6094912952
tan <t.songglod@gmail.com>
2021-10-22 18:16:53 +0700

    [EDIT] update a lot of file

---
+++ movie_industry/data_analysis/analysis_stage.R
@@ -0,0 +1,25 @@
+packages <- c("dplyr", "readr")
+lapply(packages, library, character.only = T)
+
+movies <- read_csv(file = "./movie_industry/csv/cleaned_dataset/movies_cleaned.csv")
+
+# observe dataset and data type
+glimpse(movies)
+
+# convert data type
+movies <- movies %>%
+    mutate(year = as.factor(format(year, format = "%Y"))) %>%
+    mutate(released.country = as.factor(released.country)) %>%
+    mutate_at(.vars = c("genre", "rating", "country"), .funs = as.factor) %>%
+    mutate_at(.vars = c("votes", "runtime"), .funs = as.integer)
+
+glimpse(movies)
+
+# check N/A
+colSums(is.na(movies))
+
+# analysis section
+
+# 1
+# 2
+# 3
--- movie_industry/data_exploration/explore_stage.R
+++ movie_industry/data_exploration/explore_stage.R
@@ -3,6 +3,7 @@ lapply(packages, library, character.only = T)
 
 movies <- read_csv(file = "./movie_industry/csv/cleaned_dataset/movies_cleaned.csv")
 
+# observe dataset and data type
 glimpse(movies)
 
 # convert data type
@@ -11,9 +12,15 @@ movies <- movies %>%
     mutate(released.country = as.factor(released.country)) %>%
     mutate_at(.vars = c("genre", "rating", "country"), .funs = as.factor) %>%
     mutate_at(.vars = c("votes", "runtime"), .funs = as.integer)
-    
+
+# explore section
 glimpse(movies)
+
+# check the top of dataset
 head(movies)
+
+# summary all of data
 summary(movies)
 
+# check N/A
 colSums(is.na(movies))
